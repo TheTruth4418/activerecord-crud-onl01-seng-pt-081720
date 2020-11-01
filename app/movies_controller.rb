68c6d5fd9ce7bb2d774f0d456589340435b2d2b4
William West <wwest466@gmail.com>
2020-11-01 18:32:32 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -27,8 +27,8 @@ def can_be_created_in_a_block(title = "Home Alone", release_date = 1990)
   aye = Movie.create do |m|
     m.title = title
     m.release_date = 1990
-    binding.pry
   end
+  binding.pry
 end
 
 def can_get_the_first_item_in_the_database
