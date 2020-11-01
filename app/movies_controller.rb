72b83844664243c866da165ea828bb5a4d99d98e
William West <wwest466@gmail.com>
2020-11-01 18:26:35 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -24,8 +24,8 @@ end
 
 def can_be_created_in_a_block(title = "Home Alone", release_date = 1990)
   Movie.create do |m|
-    m.string :title = title
-    m.integer :release_date = release_date
+    m.string = title
+    m.integer = release_date
   end
 end
 
