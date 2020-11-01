5382be7e3577fb6f6927c0157a1842e7246ae3b4
William West <wwest466@gmail.com>
2020-11-01 17:20:42 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -8,7 +8,7 @@
 def can_be_instantiated_and_then_saved
   movie = Movie.new
   movie.title = "This is a title."
-  movie.save
+  __
 end
 
 def can_be_created_with_a_hash_of_attributes
@@ -20,34 +20,33 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = Movie.new(attributes)
+  movie = __
 end
 
-def can_be_created_in_a_block
+def can_be_created_in_a_block(args = __)
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990
-
+  
   Movie.create do |m|
-    m.title = "Home Alone"
-    m.release_date = 1990
+    __
   end
 end
 
 def can_get_the_first_item_in_the_database
-  Movie.first.title
+  __
 end
 
 def can_get_the_last_item_in_the_database
-  Movie.last.title
+  __
 end
 
 def can_get_size_of_the_database
-  Movie.all.size
+  __
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  Movie.find_by(id: 1).title
+  __
 end
 
 def can_find_by_multiple_attributes
@@ -55,11 +54,11 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  Movie.find_by(title: "title")
+  __
 end
 
 def can_find_using_where_clause_and_be_sorted
-  # For this test return all movies released after 2002 and ordered by
+  # For this test return all movies released after 2002 and ordered by 
   # release date descending
   __
 end
