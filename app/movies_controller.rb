d9c728c5f8deec383d482dc257b7e80b12d77171
William West <wwest466@gmail.com>
2020-11-01 18:32:19 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -24,7 +24,7 @@ def can_be_created_with_a_hash_of_attributes
 end
 
 def can_be_created_in_a_block(title = "Home Alone", release_date = 1990)
-  Movie.create do |m|
+  aye = Movie.create do |m|
     m.title = title
     m.release_date = 1990
     binding.pry
